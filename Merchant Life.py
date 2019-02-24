930b8b81e6b873414d097c058bfa31bef26d6cea
SebastianW1991 <sebastianw1991@gmail.com>
2019-02-24 14:38:44 +0100

    Update Merchant Life.py

--- Merchant Life.py
+++ Merchant Life.py
@@ -5,21 +5,14 @@ rentPriceX = float(1.00)
 horseFoodX = float(2.00)
 print("Hello")
 name = input("Please, insert your name ")
-beginning = input("Hello " + name + ", after long time spent on foreign university, your uncle sent "
-                  "you a letter with horrible news"
-                  "\ndue to recent disease your father and mother deceased. What is worse, after their death, "
-                  "your home was caught by fire \nAll what remained is pair of old horses, "
-                  "old cart  and 50 feet of linen cloth for sale and 40 guldens and 30 talars."
-                  "\nFinally you reclaimed what you possess. What will you do?"
-                  " You are next to your old home and you feel saddened. "
-                  "You can:\n[a] Go to market,\n[b] Go to tavern, "
-                  "\n[c]Leave the city.\nChoose your option by choosing letters ")
+beginning = open('beginning.txt').read()
+beginning = input("Hello " + name + beginning + "[a] Go to market,\n [b] Go to tavern, \n [c] Leave the city."
+                  "\nChoose your option by choosing letters above")
 while beginning:
 
     if beginning == "a":
-        marketX = input("Welcome on the market of Xarthas, what you plan to do? \n[a] Go to horse merchant,\n"
-                        "[b] Go to armor merchant,\n[c] Go to wine merchant,\n[d] Go to weapon merchant "
-                        "\n[e] Go to wheat merchant \n[f] Try to sell your goods")
+        marketX = open('marketX.txt').read()
+        marketX = input(marketX)
         if marketX == "a":
             print("Greetings, are you looking for new steed?")
         elif marketX == "b":
@@ -33,11 +26,8 @@ while beginning:
         elif marketX == "f":
             print("You set up your stool and wait for buyer of your stuff")
     elif beginning == "b":
-        tavernX = input("Welcome in Hanging Bear. You can: \n [a] buy a beer for 5 talars. \n "
-                        "[b] buy food for you for 15 talars .\n "
-                        "[c] rent a room for 1 gulden. \n [d] buy food for your horses for 2 guldens. \n "
-                        "[e] leave tavern.")
-
+        tavernX = open('tavernX.txt').read()
+        tavernX = input(tavernX)
         if tavernX == "a":
             totalMoney = totalMoney - beerPriceX
             tavernXa = input("You feel happier, yet your pocket is lighter. You have " + str(totalMoney) + " total"
